c142c8cd00fd99c7f5d746dfaa70b90185890689
Fragoso <hfragoso@MXN1800100-2.local>
2020-02-04 20:59:45 -0600

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Fragoso, Hector on 2/4/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
